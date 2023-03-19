bc2ae10a7ca54135d9d476495bb500404bb4cdb2
Oleg T <light_ln2@hotmail.com>
2023-03-19 21:47:12 +0100

    StreamVInt implementation

--- SlidingTilePuzzle/StreamVInt.cpp
+++ SlidingTilePuzzle/StreamVInt.cpp
@@ -12,127 +12,101 @@ static struct SVEnc {
     uint8_t descriptor;
 };
 
-static std::vector<SVEnc> InitEncodingMask() {
+static struct SVEncData {
+    std::vector<SVEnc> result;
+    __m128i preshuffle;
+};
+
+static struct SVDec {
+    __m128i shuffleMask;
+    int length;
+};
+
+static SVEncData InitEncodingMask() {
     std::vector<SVEnc> result;
     result.reserve(65536);
     uint8_t data[16];
-    for (int mask = 0; mask < 65536; mask++) {
+    for (int mask = 0; mask < (1 << 12); mask++) {
         SVEnc val;
         auto mbit = [&](int index) {
             return (mask >> index) & 1;
         };
         val.descriptor = 0;
-        val.length = 0;
+        val.length = 1;
         int pos = 0;
-        if (mbit(3)) {
-            val.descriptor |= 3;
-            val.length += 4;
-            data[pos++] = 0;
-            data[pos++] = 1;
-            data[pos++] = 2;
-            data[pos++] = 3;
+        for (int i = 0; i < 4; i++) {
+            if (mbit(3 * i + 2)) {
+                val.descriptor |= 3 << (2 * i);
+                val.length += 4;
+                data[pos++] = 4 * i + 0;
+                data[pos++] = 4 * i + 1;
+                data[pos++] = 4 * i + 2;
+                data[pos++] = 4 * i + 3;
+            }
+            else if (mbit(3 * i + 1)) {
+                val.descriptor |= 2 << (2 * i);
+                val.length += 3;
+                data[pos++] = 4 * i + 0;
+                data[pos++] = 4 * i + 1;
+                data[pos++] = 4 * i + 2;
+            }
+            else if (mbit(3 * i + 0)) {
+                val.descriptor |= 1 << (2 * i);
+                val.length += 2;
+                data[pos++] = 4 * i + 0;
+                data[pos++] = 4 * i + 1;
+            }
+            else {
+                val.descriptor |= 0 << (2 * i);
+                val.length += 1;
+                data[pos++] = 4 * i + 0;
+            }
         }
-        else if (mbit(2)) {
-            val.descriptor |= 2;
-            val.length += 3;
-            data[pos++] = 0;
-            data[pos++] = 1;
-            data[pos++] = 2;
-        }
-        else if (mbit(1)) {
-            val.descriptor |= 1;
-            val.length += 2;
-            data[pos++] = 0;
-            data[pos++] = 1;
-        }
-        else {
-            val.descriptor |= 0;
-            val.length += 1;
-            data[pos++] = 0;
-        }
-
-        if (mbit(7)) {
-            val.descriptor |= 3 << 2;
-            val.length += 4;
-            data[pos++] = 4;
-            data[pos++] = 5;
-            data[pos++] = 6;
-            data[pos++] = 7;
-        }
-        else if (mbit(6)) {
-            val.descriptor |= 2 << 2;
-            val.length += 3;
-            data[pos++] = 4;
-            data[pos++] = 5;
-            data[pos++] = 6;
-        }
-        else if (mbit(5)) {
-            val.descriptor |= 1 << 2;
-            val.length += 2;
-            data[pos++] = 4;
-            data[pos++] = 5;
-        }
-        else {
-            val.descriptor |= 0 << 2;
-            val.length += 1;
-            data[pos++] = 4;
-        }
-
-        if (mbit(11)) {
-            val.descriptor |= 3 << 4;
-            val.length += 4;
-            data[pos++] = 8;
-            data[pos++] = 9;
-            data[pos++] = 10;
-            data[pos++] = 11;
-        }
-        else if (mbit(10)) {
-            val.descriptor |= 2 << 4;
-            val.length += 3;
-            data[pos++] = 8;
-            data[pos++] = 9;
-            data[pos++] = 10;
-        }
-        else if (mbit(9)) {
-            val.descriptor |= 1 << 4;
-            val.length += 2;
-            data[pos++] = 8;
-            data[pos++] = 9;
-        }
-        else {
-            val.descriptor |= 0 << 4;
-            val.length += 1;
-            data[pos++] = 8;
-        }
-
-        if (mbit(15)) {
-            val.descriptor |= 3 << 6;
-            val.length += 4;
-            data[pos++] = 12;
-            data[pos++] = 13;
-            data[pos++] = 14;
-            data[pos++] = 15;
-        }
-        else if (mbit(14)) {
-            val.descriptor |= 2 << 6;
-            val.length += 3;
-            data[pos++] = 12;
-            data[pos++] = 13;
-            data[pos++] = 14;
-        }
-        else if (mbit(13)) {
-            val.descriptor |= 1 << 6;
-            val.length += 2;
-            data[pos++] = 12;
-            data[pos++] = 13;
-        }
-        else {
-            val.descriptor |= 0 << 6;
-            val.length += 1;
-            data[pos++] = 12;
-        }
-
         while (pos < 16) data[pos++] = 255;
+        val.shuffleMask = _mm_loadu_epi8(data);
+        result.push_back(val);
+    }
+    
+    __m128i preshuffle = _mm_set_epi8(-1, -1, -1, -1, 15, 14, 13, 11, 10, 9, 7, 6, 5, 3, 2, 1);
+    return { result, preshuffle };
+}
+
+static std::vector<SVDec> InitDecodingMask() {
+    std::vector<SVDec> result;
+    result.reserve(256);
+    uint8_t data[16];
+    for (int desc = 0; desc < 256; desc++) {
+        SVDec val;
+        val.length = 1;
+        int pos = 0;
+        for (int i = 0; i < 4; i++) {
+            int bytes = 1 + (desc >> (i * 2)) % 4;
+            val.length += bytes;
+            if (bytes == 4) {
+                data[4 * i + 0] = pos++;
+                data[4 * i + 1] = pos++;
+                data[4 * i + 2] = pos++;
+                data[4 * i + 3] = pos++;
+            }
+            else if (bytes == 3) {
+                data[4 * i + 0] = pos++;
+                data[4 * i + 1] = pos++;
+                data[4 * i + 2] = pos++;
+                data[4 * i + 3] = -1;
+            }
+            else if (bytes == 2) {
+                data[4 * i + 0] = pos++;
+                data[4 * i + 1] = pos++;
+                data[4 * i + 2] = -1;
+                data[4 * i + 3] = -1;
+            }
+            else {
+                data[4 * i + 0] = pos++;
+                data[4 * i + 1] = -1;
+                data[4 * i + 2] = -1;
+                data[4 * i + 3] = -1;
+            }
+        }
         val.shuffleMask = _mm_loadu_epi8(data);
         result.push_back(val);
     }
@@ -140,43 +114,26 @@ static std::vector<SVEnc> InitEncodingMask() {
 }
 
 __forceinline static int _EncodeTuple(const uint32_t* indexes, uint8_t* buffer) {
-    static std::vector<SVEnc> precalc = InitEncodingMask();
+    static SVEncData precalc = InitEncodingMask();
     __m128i data = _mm_loadu_epi32(indexes);
-    __m128i cmpdata = _mm_cmpeq_epi8(data, _mm_max_epu8(data, _mm_set1_epi8(1)));
+    __m128i pre_data = _mm_shuffle_epi8(data, precalc.preshuffle);
+    __m128i cmpdata = _mm_cmpeq_epi8(pre_data, _mm_max_epu8(pre_data, _mm_set1_epi8(1)));
     int mask = _mm_movemask_epi8(cmpdata);
-    auto& val = precalc[mask];
+    auto& val = precalc.result[mask];
     __m128i result = _mm_shuffle_epi8(data, val.shuffleMask);
     buffer[0] = val.descriptor;
     *(__m128i*)(buffer + 1) = result;
-    return 1 + val.length;
+    return val.length;
 }
 
 __forceinline static int _DecodeTuple(const uint8_t* buffer, uint32_t* indexes) {
+    static std::vector<SVDec> precalc = InitDecodingMask();
     int descriptor = buffer[0];
-    int pos = 1;
-    for (int i = 0; i < 4; i++) {
-        int index = *(uint32_t*)&buffer[pos];
-
-        int bytesCnt = 1 + ((descriptor >> (i * 2)) & 3);
-
-        if (bytesCnt == 4) {
-            pos += 4;
-        }
-        else if (bytesCnt == 3) {
-            pos += 3;
-            index &= 0xFFFFFF;
-        }
-        else if (bytesCnt == 2) {
-            pos += 2;
-            index &= 0xFFFF;
-        }
-        else {
-            pos += 1;
-            index &= 0xFF;
-        }
-        indexes[i] = index;
-    }
-    return pos;
+    auto& val = precalc[descriptor];
+    __m128i data = _mm_loadu_epi8(buffer + 1);
+    __m128i result = _mm_shuffle_epi8(data, val.shuffleMask);
+    _mm_storeu_epi8(indexes, result);
+    return val.length;
 }
 
 __forceinline static void _EncodeBoundsTuple(const uint8_t* bounds, uint8_t* buffer) {
@@ -208,7 +165,7 @@ void StreamVInt::DecodeBoundsTuple(const uint8_t* buffer, uint8_t* bounds) {
 int StreamVInt::EncodeIndexes(int count, const uint32_t* indexes, uint8_t* buffer) {
     int pos = 0;
     for (int i = 0; i < count; i += 4) {
-        pos += EncodeTuple(&indexes[i], &buffer[pos]);
+        pos += _EncodeTuple(&indexes[i], &buffer[pos]);
     }
     return pos;
 }
@@ -216,21 +173,21 @@ int StreamVInt::EncodeIndexes(int count, const uint32_t* indexes, uint8_t* buffe
 int StreamVInt::DecodeIndexes(int count, const uint8_t* buffer, uint32_t* indexes) {
     int pos = 0;
     for (int i = 0; i < count; i += 4) {
-        pos += DecodeTuple(&buffer[pos], &indexes[i]);
+        pos += _DecodeTuple(&buffer[pos], &indexes[i]);
     }
     return pos;
 }
 
 int StreamVInt::EncodeBounds(int count, const uint8_t* bounds, uint8_t* buffer) {
     for (int i = 0; i < count; i += 8) {
-        EncodeBoundsTuple(&bounds[i], &buffer[i / 2]);
+        _EncodeBoundsTuple(&bounds[i], &buffer[i / 2]);
     }
     return count / 2;
 }
 
 int StreamVInt::DecodeBounds(int count, const uint8_t* buffer, uint8_t* bounds) {
     for (int i = 0; i < count; i += 8) {
-        DecodeBoundsTuple(&buffer[i / 2], &bounds[i]);
+        _DecodeBoundsTuple(&buffer[i / 2], &bounds[i]);
     }
     return count / 2;
 }
