3dc300afdc37189d851fdb09cca559399560e5e5
Oleg T <light_ln2@hotmail.com>
2023-04-10 11:03:39 +0200

    shorter file names

--- Solver/main.cpp
+++ Solver/main.cpp
@@ -41,36 +41,30 @@ void MTFrontierSearch() {
 
     MTSearchOptions opts;
     opts.Threads = 5;
-
     /*
-    opts.FileFrontier1 = { "e:/PUZ/frontier1", "h:/PUZ/frontier1" };
-    opts.FileFrontier2 = { "h:/PUZ/frontier2", "f:/PUZ/frontier2" };
-    opts.FileExpandedUp1 = { "g:/PUZ/expandedUp1" };
-    opts.FileExpandedDown1 = { "g:/PUZ/expandedDown1" };
-    opts.FileExpandedUp2 = { "g:/PUZ/expandedUp2" };
-    opts.FileExpandedDown2 = { "g:/PUZ/expandedDown2" };
+    opts.FileFrontierVert1 = { "e:/PUZ/fv1", "f:/PUZ/fv1", "h:/PUZ/fv1.p1", "h:/PUZ/fv1.p2" };
+    opts.FileFrontierHoriz1 = { "f:/PUZ/fh1", "h:/PUZ/fh1.p1", "h:/PUZ/fh1.p2", "e:/PUZ/fh1" };
+    opts.FileFrontierVert2 = { "h:/PUZ/fv2.p1", "h:/PUZ/fv2.p2", "e:/PUZ/fv2", "f:/PUZ/fv2" };
+    opts.FileFrontierHoriz2 = { "h:/PUZ/fh2.p2", "e:/PUZ/fh2", "f:/PUZ/fh2", "h:/PUZ/fh2.p1" };
 
-    opts.FileFrontierVert1 = { "e:/PUZ/frontier-vert-1", "f:/PUZ/frontier-vert-1" };
-    opts.FileFrontierHoriz1 = { "f:/PUZ/frontier-horiz-1", "e:/PUZ/frontier-horiz-1" };
-    opts.FileFrontierVert2 = { "h:/PUZ/frontier-vert-2" };
-    opts.FileFrontierHoriz2 = { "h:/PUZ/frontier-horiz-2" };
-    opts.FileExpanded1 = { "g:/PUZ/frontierExp1.part1", "g:/PUZ/frontierExp1.part2", "g:/PUZ/frontierExp1.part3", "g:/PUZ/frontierExp1.part4", "g:/PUZ/frontierExp1.part5" };
-    opts.FileExpanded2 = { "g:/PUZ/frontierExp2.part1", "g:/PUZ/frontierExp2.part2", "g:/PUZ/frontierExp2.part3", "g:/PUZ/frontierExp2.part4", "g:/PUZ/frontierExp2.part5" };
+    opts.FileExpanded1 = { "g:/PUZ/e1.part1", "g:/PUZ/e1.part2", "g:/PUZ/e1.part3", "g:/PUZ/e1.part4", "g:/PUZ/e1.part5" };
+    opts.FileExpanded2 = { "g:/PUZ/e2.part1", "g:/PUZ/e2.part2", "g:/PUZ/e2.part3", "g:/PUZ/e2.part4", "g:/PUZ/e2.part5" };
     opts.ExpandedFileSequentialParts = true;
     */
-
-    opts.FileFrontierVert1 = { "d:/PUZ/frontier-vert-1" };
-    opts.FileFrontierHoriz1 = { "d:/PUZ/frontier-horiz-1" };
-    opts.FileFrontierVert2 = { "d:/PUZ/frontier-vert-2" };
-    opts.FileFrontierHoriz2 = { "d:/PUZ/frontier-horiz-2" };
-    opts.FileExpanded1 = { "c:/PUZ/frontierExp1.part1", "c:/PUZ/frontierExp1.part2", "c:/PUZ/frontierExp1.part3", "c:/PUZ/frontierExp1.part4", "c:/PUZ/frontierExp1.part5" };
-    opts.FileExpanded2 = { "c:/PUZ/frontierExp2.part1", "c:/PUZ/frontierExp2.part2", "c:/PUZ/frontierExp2.part3", "c:/PUZ/frontierExp2.part4", "c:/PUZ/frontierExp2.part5" };
+    
+    opts.FileFrontierVert1 = { "d:/PUZ/fv1" };
+    opts.FileFrontierHoriz1 = { "d:/PUZ/fh1" };
+    opts.FileFrontierVert2 = { "d:/PUZ/fv2" };
+    opts.FileFrontierHoriz2 = { "d:/PUZ/fh2" };
+    opts.FileExpanded1 = { "c:/PUZ/e1.part1", "c:/PUZ/e1.part2", "c:/PUZ/e1.part3", "c:/PUZ/e1.part4", "c:/PUZ/e1.part5" };
+    opts.FileExpanded2 = { "c:/PUZ/e2.part1", "c:/PUZ/e2.part2", "c:/PUZ/e2.part3", "c:/PUZ/e2.part4", "c:/PUZ/e2.part5" };
     opts.ExpandedFileSequentialParts = true;
 
-    //opts.MaxDepth = 17;
+    opts.MaxDepth = 20;
+    
 
     try {
-        MTFrontierSearch<7, 2>(opts);
+        MTFrontierSearch<8, 2>(opts);
     }
     catch (const std::exception& ex) {
         std::cerr << "Error: " << ex.what() << std::endl;
